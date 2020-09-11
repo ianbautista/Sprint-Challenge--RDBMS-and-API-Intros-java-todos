66c11575335f828c85433ece083d78ac79f895de
ianbautista <ianfbautista@gmail.com>
2020-09-11 08:10:33 -0700

    added relationship to Todos. Generated getter and setter.

--- todos/src/main/java/com/lambdaschool/todos/models/User.java
+++ todos/src/main/java/com/lambdaschool/todos/models/User.java
@@ -6,7 +6,9 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * The entity allowing interaction with the users table
@@ -43,6 +45,11 @@ public class User extends Auditable
         unique = true)
     @Email
     private String primaryemail;
+
+    //relationship to Todos
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties(value = "user")
+    private Set<Todos> todos = new HashSet<>();
 
     /**
      * Default constructor used primarily by the JPA.
@@ -160,5 +167,14 @@ public class User extends Auditable
     public void setPassword(String password)
     {
         this.password = password;
+    }
+
+    // Todos getter and setter
+    public Set<Todos> getTodos() {
+        return todos;
+    }
+
+    public void setTodos(Set<Todos> todos) {
+        this.todos = todos;
     }
 }
